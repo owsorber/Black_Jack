f5da8eb8cc23ecd8535c8a009208bf814324dc34
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-10-23 09:20:02 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,14 @@
+/**
+ * A class built to output the Game class's features to the user.
+ *
+ * Owen Sorber
+ * 3/6/18
+ */
+import java.util.ArrayList;
+public class Driver
+{
+    public static void main (String[] args) {
+        Game game = new Game(3);
+        game.play();
+    }
+}
